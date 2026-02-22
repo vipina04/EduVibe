ca189b5889b62fc85377da2da811b2ef72a993d4
vipina <vipina184gmail.com>
2026-02-22 18:52:58 +0530

    updated user model from 20 to 30 char value

--- users/models.py
+++ users/models.py
@@ -30,7 +30,7 @@ class CustomUser(AbstractUser):
     dob = models.DateField(null=True, blank=True)
 
     unique_id = models.CharField(
-        max_length=20,
+        max_length=30,
         unique=True,
         blank=True,
         null=True
